a1cee4a9ddfc2cbc5c5e965aaf96e587b02422bb
willesxm <willeslau@gmail.com>
2021-10-24 11:00:40 +0800

    simple benchmark

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "src/btree.h"
+#include <time.h>
 
 int intKeyCompare(const void* key1, const void* key2) {
     int v = *((int*)key1) - *((int*)key2);
@@ -11,7 +12,7 @@ void printIntKeyValue(void* key, void* val) {
     else printf("(%d, %d) -> ", *(int*)key, *(int*)val);
 }
 
-int main() {
+void sanityCheck() {
     Btree* btree = btreeCreate(intKeyCompare);
 
     int k1 = 2, v1 = 2;
@@ -45,6 +46,37 @@ int main() {
 
     printElements(btree, printIntKeyValue);
     printf("tree height: %d\n", btree->height);
+}
 
+typedef struct Key {
+    int key;
+} Key;
+
+int keyCompare(const void* key1, const void* key2) {
+    return ((Key*)key1)->key - ((Key*)key2)->key;
+}
+
+void speed() {
+    unsigned int size = 10000;
+    void** keys = malloc(sizeof(void*) * size);
+    for (int i = 0; i < size; i++) {
+        Key* key = malloc(sizeof(Key));
+        key->key = i;
+        keys[i] = (void*) key;
+    }
+
+    Btree* btree = btreeCreate(keyCompare);
+    clock_t start_time = clock();
+    for (int i = 0; i < size; i++) {
+        insert(btree, keys[i], keys[i]);
+    }
+    clock_t elapsed_time = clock() - start_time;
+    printf("Done %d inserts in %f seconds. ", size, (double)elapsed_time / CLOCKS_PER_SEC);
+    printf("Per loop time: %f ms\n", 1000 * (double)elapsed_time / size / CLOCKS_PER_SEC);
+}
+
+int main() {
+//    sanityCheck();
+    speed();
     return 0;
 }
